52cd0160459138634e4061f0e5fffb06f9fd6c94
jaykay99992 <jasemkhelifi@gmail.com>
2023-11-21 14:22:35 -0500

    merged two for loops into one

--- src/flask/app.py
+++ src/flask/app.py
@@ -1228,21 +1228,22 @@ class Flask(App):
         further request handling is stopped.
         """
         names = (None, *reversed(request.blueprints))
+        result = None
 
         for name in names:
             if name in self.url_value_preprocessors:
                 for url_func in self.url_value_preprocessors[name]:
                     url_func(request.endpoint, request.view_args)
 
-        for name in names:
             if name in self.before_request_funcs:
                 for before_func in self.before_request_funcs[name]:
                     rv = self.ensure_sync(before_func)()
 
                     if rv is not None:
-                        return rv
+                        result = rv
 
-        return None
+
+        return result
 
     def process_response(self, response: Response) -> Response:
         """Can be overridden in order to modify the response object
